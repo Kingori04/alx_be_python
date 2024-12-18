0891c5721ece268e02a2abb13ce96090f25c11a5
Dennis Ngatia <ngatiaalx@gmail.com>
2024-12-18 05:18:03 +0300

    Add correctionj for finance_calculator.py for monthly and annual savings calculation

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -4,13 +4,13 @@
 monthly_income = float(input("Enter your monthly income: "))
 
 # Prompt the user for total monthly expenses
-total_expenses = float(input("Enter your total monthly expenses: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 # Calculate monthly savings
-monthly_savings = monthly_income - total_expenses
+monthly_savings = monthly_income - monthly_expenses
 
 # Project annual savings with 5% interest
-annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+annual_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
 
 # Output the results
 print(f"Your monthly savings are ${monthly_savings:.2f}.")
